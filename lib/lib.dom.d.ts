92c2d7303c95ab4a591fd680f14bde60c7796dfe
Fabian Cook <faybecook@gmail.com>
2019-07-07 12:08:21 +1200

    MediaQueryList addListener & removeListener should not be deprecated
    Related to https://github.com/microsoft/TypeScript/issues/32210

--- lib/lib.dom.d.ts
+++ lib/lib.dom.d.ts
@@ -10014,9 +10014,7 @@ interface MediaQueryList extends EventTarget {
     readonly matches: boolean;
     readonly media: string;
     onchange: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) | null;
-    /** @deprecated */
     addListener(listener: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) | null): void;
-    /** @deprecated */
     removeListener(listener: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) | null): void;
     addEventListener<K extends keyof MediaQueryListEventMap>(type: K, listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
     addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
